8fcf25af9f7e0b820af47d45a2e7e21d5c386b06
Cortes, King Larce, arvato Systems Malaysia <kinglarce.cortes@bertelsmann.de>
2017-09-15 16:15:02 +0800

    databinding assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  username = '';
+  isTextboxEmpty = true;
+
+  clearTextbox(){
+    this.username = '';
+  }
+
+  checkTextbox(){
+    if(this.username){
+      this.isTextboxEmpty = false;
+      return;
+    }
+    this.isTextboxEmpty = true;
+  }
+
 }
